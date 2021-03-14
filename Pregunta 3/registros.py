822c3f1c56e542bdfdfb661e1a72049aa3c69ede
Carlos Gonzalez <carlosegl10@gmail.com>
2021-03-14 11:57:34 -0400

    Listo imprimir sin eglas

--- Pregunta 3/registros.py
+++ Pregunta 3/registros.py
@@ -7,9 +7,9 @@ class Atomico:
         self.alineacion = alineacion
 
     def __str__(self):
-        return "Tipo: {}, Rep: {}, Ali: {}".format(self.nombre,self.representacion, self.alineacion)
+        return "Soy un registro atomico de nombre: {}, mi representacion es de {} bytes y mi alineacion es de {} bytes".format(self.nombre,self.representacion, self.alineacion)
 
-class Structs:
+class Struct:
     def __init__(self, nombre, elementos, representacion= None, alineacion = None, elementos_optimo = None, desperdicio = None):
         self.nombre = nombre
         self.elementos = elementos
@@ -18,43 +18,43 @@ class Structs:
         self.desperdicio = desperdicio
         self.elementos_optimo = elementos_optimo
 
+class Union:
+    def __init__(self, nombre, elementos, representacion = None, alineacion = None):
+        self.nombre = nombre
+        self.elementos = elementos
+        self.representacion = representacion
+        self.alineacion = alineacion
+
 
 atomicos = dict()
 registros = dict()
+variantes = dict()
 
 
 
 
 def es_valida(opcion):
     opcion = opcion.split(" ")
+    if opcion[0].lower() == "salir":
+        return True
     if len(opcion) < 2:
         return False    
-    if opcion[0].lower() != "atomico" and opcion[0].lower() != "struct" and opcion[0].lower() != "describir": 
+    if opcion[0].lower() != "atomico" and opcion[0].lower() != "struct" and opcion[0].lower() != "describir" and opcion[0].lower() != "union": 
         return False
     if opcion[0].lower() == "atomico":
         if(len(opcion) > 4):            
             return False
         if opcion[2].isdigit() and opcion[3].isdigit():
             return True
-    if opcion[0].lower() == "struct":
+    if opcion[0].lower() == "struct" or opcion[0].lower() == "union":
         if len(opcion) < 3:
             return False
         return True
     if opcion[0].lower() == "describir":
         return True
 
-def print_atomicos():
-    for key, value in atomicos.items():
-        print( key,value)
-
-def print_structs():
-    for key, values in registros.items():
-        print("Struct:", key)
-        for value in values:
-            print(value)
-
 def ya_existente(opcion):
-    if (opcion[1] in atomicos.keys() or opcion[1] in registros.keys()):
+    if ( opcion[1] in atomicos.keys() or opcion[1] in registros.keys() or opcion[1] in variantes.keys() ):
         print("Nombre ya existente") 
         return True 
 
@@ -62,10 +62,10 @@ def crear_atomico(opcion):
     if(not(ya_existente(opcion))):          
         atomicos[opcion[1]] = Atomico(opcion[1],opcion[2], opcion[3])
     
-def crear_registro(opcion):
+def crear_registro(opcion, tipo):
     if(not(ya_existente(opcion))): 
         for elem in opcion[2:]: 
-            if elem not in atomicos.keys() and elem not in registros.keys():
+            if elem not in atomicos.keys() and elem not in registros.keys() and elem not in variantes.keys():
                 print("Ha introducido un tipo no existente")
                 return
         nuevos_elementos = []
@@ -74,9 +74,18 @@ def crear_registro(opcion):
             try:
                 nuevos_elementos.append(atomicos[elem])
             except:
-                nuevos_elementos.append(registros[elem])       
-        nuevo_registro = Structs(opcion[1], nuevos_elementos)
-        registros[opcion[1]] = nuevo_registro
+                try:
+                    nuevos_elementos.append(registros[elem])   
+                except:
+                    nuevos_elementos.append(variantes[elem])    
+        if(tipo == "struct"):
+            nuevo_registro = Struct(opcion[1], nuevos_elementos)
+            registros[opcion[1]] = nuevo_registro
+        else:
+            nuevo_registro = Union(opcion[1], nuevos_elementos)
+            variantes[opcion[1]] = nuevo_registro
+
+
 
 def split_array(arreglo):
     matriz = []
@@ -91,7 +100,7 @@ def calcular_tamano_y_perdida(array):
     return len(array), array.count(0)
 
 
-def crear_espacio_sin_reglas(array,resultado ):
+def crear_espacio_sin_reglas(array, resultado):
     array = array[::-1]
 
     while array:
@@ -99,9 +108,9 @@ def crear_espacio_sin_reglas(array,resultado ):
         counter = int(elemento.representacion)
         nombre = elemento.nombre
         while counter:
-            resultado.append(nombre)
+            resultado.append(elemento)
             counter -= 1
-    return resultado
+    return len(resultado), resultado
   #  while len(resultado) % 4 != 0:
   #      resultado.append(0)
 
@@ -157,24 +166,6 @@ def crear_espacio_con_reglas(array, resultado):
 
 
 
-   # while array:
-   #     elemento = array.pop()
-   ##     counter = int(elemento.representacion)
-    #    tamano = 0
-        
-    #    nombre = elemento.nombre
-      #  while counter != 0:
-       #     if counter % 4 == 0:
-       #         resultado.append([0,0,0,0])
-       #         for j in range(counter):
-       #             resultado[len(resultado)-1][j] = nombre
-         
-
-
-      #  if i % 4 == 0:
-      #      resultado.append([0,0,0,0])
-      #      for j in range(3):
-      #      resultado[len(resultado)-1][j] = nombre
 
 def verificar_nombres(elem, array):
     for elemento in array:
@@ -187,44 +178,14 @@ def verificar_hay_struct(array):
         if elem.nombre in registros.keys():
             resultado.append(elem)
     return resultado
-#def multiples_opciones(elemento, resultado, arreglo, ya_evaluados, recursion_level):
-       
-#        counter = int(elemento.representacion)
-#        alineacion = int(elemento.alineacion)
-#        nombre = elemento.nombre        
-#        if not(resultado):
-#            while counter:
-#                resultado.append(nombre)
-#                counter -= 1
-#        else:
-#            if len(resultado) % alineacion == 0:
-#                print(elemento)
-#                while counter:
-#                    resultado.append(nombre)
-#                    counter -= 1
-#            else:
-#                print("extendiendo con 0s", elemento)
-#                while len(resultado) % alineacion != 0 :
-#                    resultado.append(0)
-#                while counter:
-                    
- #                   resultado.append(nombre)
- #                   counter -= 1
-                    
-#        ya_evaluados.append(elemento)
-        
-#        for elem in arreglo:           
-#            if not(verificar_nombres(elem, ya_evaluados)):                
-#                print("holi soy un elem", elem,"mi padre es:", elemento, "recursion level:", recursion_level)
-#                multiples_opciones(elem, resultado, arreglo, ya_evaluados, recursion_level+1)
-#        print(resultado)
-#        return resultado
-                
-                
-   # while len(resultado) % 4 != 0:
-   #     resultado.append(0)
-   # print(resultado)
-   # split_array(resultado)
+
+def verificar_hay_union(array):
+    resultado = []
+    for elem in array:
+        if elem.nombre in variantes.keys():
+            resultado.append(elem)
+    return resultado
+
 
 def determina_optimo(array):
     
@@ -245,45 +206,61 @@ def backtracking(nombre):
             resultado.append(webito)
     return determina_optimo(resultado)
 
-def equals_cero(x):
-    return x == 0
-
-def revisar_ceros(array, elem):
-   # array = array[::-1]
-    resultado = dict()
-    i = 0
-    while i< len(array):
-        x = array[i-1]
-        if(array[i] == 0):
-            contador = 0
-            while array[i] == 0:
-                contador += 1
-                i += 1
-            resultado[x] = contador
-        else:
-            i += 1
-    return resultado
-    
-  #  i = 0
-  #  while i < len(array):
-  #      if array[i] != 0:
-  #          i = i+1
-  #          continue
-  #      nuevo_array = list(takewhile(equals_cero, array[i:]))
+def tamano_union(array):
+    resultado = []
+    for elem in array:
+        resultado.append(elem.representacion)
+    return max(resultado)
+def imprimir_array_como_matriz(array):
+    for i in range(len(array)):
+        if i % 4 == 0 : print()
+        try:
+            print(array[i].nombre + " |", end=" ")
+        except:
+            print(array[i])
         
-  #      i = len(nuevo_array) + i
 
-  #      resultado[array[len(nuevo_array)-i-1]] = len(nuevo_array)
-  #  print(resultado)
-   
+
+def print_sin_reglas(array, imprimir, interno, acumulado):
+    indice = 0
+    while indice < len(array):
+        if(interno):
+                print("\t", end="")
+        if array[indice].nombre in atomicos.keys():
+            print("Me encuentro en la posicion {}, {}".format(indice+acumulado, array[indice]))
+            indice += int(array[indice].representacion)
+
+        elif array[indice].nombre in registros.keys():
+            print("Soy un struct, empiezo en la posicion {}, ocupo {} bytes".format(indice+acumulado, array[indice].representacion ))
+            
+            print_sin_reglas(array[indice].elementos_optimo, False, True, indice)
+            indice += int(array[indice].representacion)
+        elif array[indice].nombre in variantes.keys():
+            print("Soy un union, empiezo en la posicion {}, ocupo {} bytes".format(indice+acumulado, array[indice].representacion))
+            indice += int(array[indice].representacion)
+    if imprimir:
+        imprimir_array_como_matriz(array)
+
 def describir(nombre):
     
-    if nombre in registros.keys():
+    if nombre in atomicos.keys():
+        print(atomicos[nombre])
+
+    elif nombre in registros.keys():
         a_evaluar = registros[nombre].elementos
         #Espacio sin reglas:
         structs = verificar_hay_struct(a_evaluar)
-
-
+        unions = verificar_hay_union(a_evaluar)
+        #Caso Struct sin reglas
+        if(structs):
+            for elem in structs:
+                elem.representacion, elem.elementos_optimo = crear_espacio_sin_reglas(elem.elementos, []) 
+        if(unions):
+            for elem in unions:
+                elem.representacion = tamano_union(elem.elementos)
+        
+        registros[nombre].representacion, registros[nombre].elementos_optimo = crear_espacio_sin_reglas(a_evaluar, [])
+        print_sin_reglas(registros[nombre].elementos_optimo, True, False, 0)
       #  if(structs):
       #      for elem in structs:
                 #elem.representacion = len(crear_espacio_sin_reglas(elem.elementos, []))
@@ -292,24 +269,24 @@ def describir(nombre):
       #          elem.alineacion = elem.elementos[0].alineacion
             
       #  print(crear_espacio_con_reglas(a_evaluar, []))
-        if(structs):
-            for elem in structs:
-                elem.elementos_optimo, elem.representacion, elem.desperdicio, elem.alineacion = backtracking(elem.nombre)
+      #  if(structs):
+      #      for elem in structs:
+      #          elem.elementos_optimo, elem.representacion, elem.desperdicio, elem.alineacion = backtracking(elem.nombre)
 
-                for webo in elem.elementos_optimo:
-                    try:
-                        print(webo.nombre)
-                    except:
-                        print(webo)
+       #         for webo in elem.elementos_optimo:
+       #             try:
+       #                 print(webo.nombre)
+       #             except:
+       #                 print(webo)
 
 
-            registros[nombre].elementos_optimo,registros[nombre].representacion,registros[nombre].desperdicio,registros[nombre].alineacion = backtracking(nombre)
-            print(registros[nombre].elementos_optimo)
-            for elem in registros[nombre].elementos_optimo:                
-                try:
-                    print(elem.nombre)
-                except:
-                    print(elem)
+       #     registros[nombre].elementos_optimo,registros[nombre].representacion,registros[nombre].desperdicio,registros[nombre].alineacion = backtracking(nombre)
+       #     print(registros[nombre].elementos_optimo)
+       #     for elem in registros[nombre].elementos_optimo:                
+       #         try:
+       #             print(elem.nombre)
+       #         except:
+       #             print(elem)
                 #
                 #
                 
@@ -332,17 +309,21 @@ def main():
     while True:
         print("Introduzca ATOMICO <nombre> <representacion> <alineacion> para crear un nuevo tipo atomico")
         print("Introduzca STRUCT <nombre> [<tipo>] para crear un nuevo struct")
+        print("Introduzca UNION <nombre> [<tipo>] para crear un nuevo struct")
         print("Introduzca DESCRIBIR <nombre> para describir un registro ya existente")
+        print()
         opcion = input()
         if(es_valida(opcion)):
             opcion = opcion.split(" ")
 
             if(opcion[0].lower() == "atomico"):                
                 crear_atomico(opcion)
-                print_atomicos()
+                
             elif(opcion[0].lower() == "struct"):
-                crear_registro(opcion)
+                crear_registro(opcion, "struct")
                # print_structs()
+            elif(opcion[0].lower() == "union"):
+                crear_registro(opcion, "union")
             elif(opcion[0].lower() == "describir"):
                 describir(opcion[1])
               
@@ -350,10 +331,7 @@ def main():
                 break
         else:
             print("Por favor introduzca una opcion valida")
-
-   
-
-
+        print()
 
 if __name__ == '__main__':
     main()
